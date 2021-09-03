ceb836a431fe592aa5918bf5e622618872ca0508
EnricoSaporiti <enrico.saporiti@gmail.com>
2021-09-03 19:29:32 +0200

    Secondo commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+'use strict';
+
 // Import package
 const express = require('express');
 
 // Create application
 const app = express();
 
-// Define routes and web pages
-app.get('/', (req, res) => res.send('Hello World!'));
+// Import module
+app.use(express.static('./public'));
+
+app.get('/login', (req, res) => {
+	const user = req.query.user || 'Unknow';
+	const pass = req.query.pass || 'Unknow';
+});
 
 // Activate server
 app.listen(3000, () => console.log('Server ready'));
