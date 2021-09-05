ccb9e9dd4391b4863fa7067b3002a316d2c0713f
EnricoSaporiti <enrico.saporiti@gmail.com>
2021-09-05 22:59:51 +0200

    terzo commit

--- index.js
+++ index.js
@@ -3,15 +3,44 @@
 // Import package
 const express = require('express');
 
+const morgan = require('morgan');
+
 // Create application
 const app = express();
 
 // Import module
+app.use(morgan('tiny'));
 app.use(express.static('./public'));
+
+const dogsList = [
+	{ id: 3, name: 'bobby' },
+	{ id: 5, name: 'wolf' },
+];
+
+app.get('/dogs', (req, res) => {
+	res.json(dogsList.map((dog) => dog.id));
+});
+
+app.get('/dogs/:dogId', (req, res) => {
+	const dogId = req.params.dogId;
+	const theDog = dogsList.filter((dog) => dog.id == dogId);
+	if (theDog.length == 1) {
+		res.json(theDog[0]);
+	} else {
+		res.status(400).json({
+			reson: 'dog not fount',
+		});
+	}
+	res.json();
+});
+
+app.post('/dogs');
 
 app.get('/login', (req, res) => {
 	const user = req.query.user || 'Unknow';
 	const pass = req.query.pass || 'Unknow';
+
+	res.send(`Login data receivedr by ${user}  password ${pass} `);
 });
 
 // Activate server
