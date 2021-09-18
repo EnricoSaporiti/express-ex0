2905a12da23ef99e6dc60f6e6e0b53a90371d32e
EnricoSaporiti <enrico.saporiti@gmail.com>
2021-09-18 18:37:09 +0200

    Commit casa gestione JSON

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ const morgan = require('morgan');
 // Create application
 const app = express();
 
+app.use(morgan('dev'));
+
 // Import module
-app.use(morgan('tiny'));
+//app.use(morgan('tiny'));
 app.use(express.static('./public'));
+app.use(express.json());
+
+app.get('/login', (req, res) => {
+	const user = req.query.user || 'Unknow';
+	const pass = req.query.pass || 'Unknow';
+
+	res.send(`Login data receivedr by ${user}  password ${pass} `);
+});
 
 const dogsList = [
 	{ id: 3, name: 'bobby' },
@@ -18,7 +28,11 @@ const dogsList = [
 ];
 
 app.get('/dogs', (req, res) => {
-	res.json(dogsList.map((dog) => dog.id));
+	res.json(
+		dogsList.map((dog) => ({
+			id: dog.id,
+		}))
+	);
 });
 
 app.get('/dogs/:dogId', (req, res) => {
@@ -34,15 +48,17 @@ app.get('/dogs/:dogId', (req, res) => {
 	res.json();
 });
 
-get.post('')
+app.post('/dogs', (req, res) => {
+	const theDog = req.body;
+	if (theDog.id && theDog.name) {
+		dogsList.push({ id: theDog.id, name: theDog.name }); // add to database
+		console.log(dogsList);
 
-app.post('/dogs');
-
-app.get('/login', (req, res) => {
-	const user = req.query.user || 'Unknow';
-	const pass = req.query.pass || 'Unknow';
-
-	res.send(`Login data receivedr by ${user}  password ${pass} `);
+		res.json(dogsList); // close the POST request with an empty body
+	} else {
+		// the Json object doesn't have the expected properties
+		res.status(400).json({ reason: 'insufficient information' });
+	}
 });
 
 // Activate server
